475d8e7f58edc5dde31eddf71ba75692f97b4387
BuildTools <unconfigured@null.spigotmc.org>
2020-08-26 00:10:34 +0200

    changed text in main.java

--- src/com/github/km127pl/javaterminal/Main.java
+++ src/com/github/km127pl/javaterminal/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner scanner = new Scanner(System.in);
 
-	    System.out.print("Enter your name: ");
+	    System.out.print("Enter the command to run: ");
 
 	    String cmd = scanner.next();
 
