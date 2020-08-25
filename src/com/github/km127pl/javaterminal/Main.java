bef1b65a3c422c9e8bd996ae286a69c869d582e7
BuildTools <unconfigured@null.spigotmc.org>
2020-08-26 00:06:34 +0200

    Added main.java

---
+++ src/com/github/km127pl/javaterminal/Main.java
@@ -0,0 +1,20 @@
+package com.github.km127pl.javaterminal;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+
+	    System.out.print("Enter your name: ");
+
+	    String cmd = scanner.next();
+
+	    System.out.println("Running: " + cmd);
+
+
+	    Runtime.getRuntime().exec(cmd);
+	    scanner.close();
+	}
+}
